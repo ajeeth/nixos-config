8ddb879c2d1e02640a3caccc9b19d0c00eb4a6d3
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-01-30 16:36:48 +0400

    Create lightdm.nix

---
+++ system/wm/lightdm.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  # Configure lightdm
+  services.xserver.displayManager = {
+    lightdm.enable = true;
+    sessionCommands = ''
+    xset -dpms
+    xset s blank
+    xset r rate 350 50
+    xset s 300
+    ${pkgs.lightlocker}/bin/light-locker --idle-hint &
+  '';
+  };
+}
