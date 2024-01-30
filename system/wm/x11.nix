27e2c7c9ade151f9d3fd79fc9bc0bbfd846acad2
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-01-30 16:37:35 +0400

    Update x11.nix

--- system/wm/x11.nix
+++ system/wm/x11.nix
@@ -3,7 +3,6 @@
 {
   imports = [ ./pipewire.nix
               ./dbus.nix
-              ./gnome-keyring.nix
               ./fonts.nix
             ];
 
@@ -14,16 +13,6 @@
     xkbVariant = "";
     xkbOptions = "caps:escape";
     excludePackages = [ pkgs.xterm ];
-    displayManager = {
-      lightdm.enable = true;
-      sessionCommands = ''
-      xset -dpms
-      xset s blank
-      xset r rate 350 50
-      xset s 300
-      ${pkgs.lightlocker}/bin/light-locker --idle-hint &
-    '';
-    };
     libinput = {
       touchpad.disableWhileTyping = true;
     };
