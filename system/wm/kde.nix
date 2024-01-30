98a253efb0d7ee53192237bcad4470b0d12d36d6
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-01-30 19:27:56 +0400

    Update kde.nix

--- system/wm/kde.nix
+++ system/wm/kde.nix
@@ -8,4 +8,35 @@
 
   # Enable the Plasma 5 Desktop Environment.
   services.xserver.desktopManager.plasma5.enable = true;
+
+  environment = {
+    systemPackages = with pkgs; [
+      # tools
+      akonadi
+      ark
+      dragon
+      filelight
+      kamoso
+      kate
+      kcalc
+      #kdenlive
+      kdiff3
+      konsole
+      kfind
+      kget
+      kgpg
+      kmplot
+      labplot
+      qttools
+      quazip
+      syncthingtray
+      tokodon
+      wacomtablet
+
+      # integration
+      kaccounts-integration
+      kaccounts-providers
+      kio-gdrive
+    ];
+  };
 }
