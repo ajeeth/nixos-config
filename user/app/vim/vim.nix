7f272948da2da260fd15145902688e1d869042d0
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-01-30 23:06:01 +0400

    Update vim.nix

--- user/app/vim/vim.nix
+++ user/app/vim/vim.nix
@@ -6,9 +6,19 @@
     settings = { ignorecase = true; };
     extraConfig = ''
       set mouse-=a
+      filetype on
+      filetype plugin on
       syntax enable
+      set grepprg=grep\ -nH\ $*
       set smartcase
       set autoindent
+      set expandtab
+      set smarttab
+      set shiftwidth=3
+      set softtabstop=3
+      set incsearch
+      set hlsearch
+      set showcmd
     '';
   };
 }
