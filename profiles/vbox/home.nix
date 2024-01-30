aef959ed3519b9b30738d99a49dbb6c0cad75347
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-01-30 20:54:48 +0400

    Update home.nix

--- profiles/vbox/home.nix
+++ profiles/vbox/home.nix
@@ -49,17 +49,12 @@
     music = "${config.home.homeDirectory}/Media/Music";
     videos = "${config.home.homeDirectory}/Media/Videos";
     pictures = "${config.home.homeDirectory}/Media/Pictures";
-    templates = "${config.home.homeDirectory}/Templates";
     download = "${config.home.homeDirectory}/Downloads";
     documents = "${config.home.homeDirectory}/Documents";
     desktop = null;
     publicShare = null;
     extraConfig = {
       XDG_DOTFILES_DIR = "${userSettings.dotfilesDir}";
-      XDG_ARCHIVE_DIR = "${config.home.homeDirectory}/Archive";
-      XDG_VM_DIR = "${config.home.homeDirectory}/Machines";
-      XDG_PODCAST_DIR = "${config.home.homeDirectory}/Media/Podcasts";
-      XDG_BOOK_DIR = "${config.home.homeDirectory}/Media/Books";
     };
   };
   xdg.mime.enable = true;
