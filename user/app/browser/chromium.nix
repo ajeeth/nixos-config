86d56a7be6ddc0768fc3afc5ce5d5c9e7162a632
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-01-31 10:32:10 +0400

    Create chromium.nix

---
+++ user/app/browser/chromium.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+{
+  programs.chromium = {
+    enable = true;
+    extensions = [
+    "cbnipbdpgcncaghphljjicfgmkonflee" # Axel Springer Blocker
+    "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+    "hjdoplcnndgiblooccencgcggcoihigg" # Terms of Service; Didn’t Read
+    #"gcbommkclmclpchllfjekcdonpmejbdp" # HTTPS Everywhere
+    "klbibkeccnjlkjkiokjodocebajanakg" # The Great Suspender
+    #"fngmhnnpilhplaeedifhccceomclgfbg" # EditThisCookie
+    "oboonakemofpalcgghocfoadofidjkkk" # KeePassXC-Browser
+    "fploionmjgeclbkemipmkogoaohcdbig" # Page load time
+    "feeoiklfggbaibpdhkkngbpkppdmcjal" # Tab Counter
+    #"kglhbbefdnlheedjiejgomgmfplipfeb" # Jitsi Meetings
+    ];
+    extraOpts = {
+    "BrowserSignin" = "0";
+    "SyncDisabled" = true;
+    "PasswordManagerEnabled" = true;
+    "BuiltInDnsClientEnabled" = false;
+    "​DeviceMetricsReportingEnabled" = false;
+    "​ReportDeviceCrashReportInfo" = true;
+    "​SpellcheckEnabled" = true;
+    "​SpellcheckLanguage" = [
+        "en-US"
+    ];
+    "​CloudPrintSubmitEnabled" = false;
+    };
+  };
+}
