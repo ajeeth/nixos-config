f388d4106fdd4a6fa128fa03b63b0c59c6b81755
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-02-01 14:11:05 +0400

    Update sddm.nix

--- system/wm/sddm.nix
+++ system/wm/sddm.nix
@@ -4,6 +4,6 @@
   # Enable SDDM.
   services.xserver.displayManager.sddm = {
     enable = true;
-    theme = "${import ../../themes/sddm/sddm-sugar-dark-c.nix { inherit pkgs; }}";
+    theme = "${import ../../themes/sddm/sddm-sugar-light.nix { inherit pkgs; }}";
   };
 }
