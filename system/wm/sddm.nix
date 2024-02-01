5a9442c7bb377f189ae885dee8e2b56dd762ff43
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-02-01 17:27:49 +0400

    Update sddm.nix

--- system/wm/sddm.nix
+++ system/wm/sddm.nix
@@ -5,7 +5,7 @@
   services.xserver.displayManager.sddm = {
     enable = true;
     theme = "${import ../../themes/sddm/where-is-my-sddm-theme.nix { inherit pkgs; }}";
-    extraConfig = ''
+    settings = ''
       [General]
       InputMethod=
     '';
