de8a8e85378a5c518aacdc6c3a6b427df7b7d46d
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-02-01 13:53:54 +0400

    Update sddm-sugar-dark-c.nix

--- themes/sddm/sddm-sugar-dark-c.nix
+++ themes/sddm/sddm-sugar-dark-c.nix
@@ -2,12 +2,14 @@
 
 let
   #imgLink = "https://images.wallpaperscraft.com/image/single/village_houses_mountain_193137_1920x1200.jpg";
+  #imgSha256 = "1rlvjc31c4n3dp1k7s40pxcwz5lgc5vwpbmhmh69xj581adfbgc5";
+
   imgLink = "https://images.wallpaperscraft.com/image/single/peacocks_feathers_patterns_118604_1600x1200.jpg";
+  imgSha256 = "0d586y5hk710n5zqf7ri1ijhpi8r5pgh7a02izzcgcmvcmf23xzv";
 
   image = pkgs.fetchurl {
     url = imgLink;
-    #sha256 = "1rlvjc31c4n3dp1k7s40pxcwz5lgc5vwpbmhmh69xj581adfbgc5";
-    sha256 = "0d586y5hk710n5zqf7ri1ijhpi8r5pgh7a02izzcgcmvcmf23xzv";
+    sha256 = imgSha256;
   };
 in
 pkgs.stdenv.mkDerivation {
