aab8e89e32c8e7a0ab82ae59429ce3955b4f6ec0
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-02-01 17:33:15 +0400

    Update sddm.nix

--- system/wm/sddm.nix
+++ system/wm/sddm.nix
@@ -6,7 +6,7 @@
     enable = true;
     theme = "${import ../../themes/sddm/where-is-my-sddm-theme.nix { inherit pkgs; }}";
     settings = {
-      InputMethod =
+      InputMethod = "";
     };
   };
 }
