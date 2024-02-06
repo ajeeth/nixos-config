82150182cd65023fc84b56aae595c3a1da90c7de
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-02-06 23:08:28 +0400

    Update home.nix

--- profiles/wsl/home.nix
+++ profiles/wsl/home.nix
@@ -3,45 +3,45 @@
 {
   imports = [
               ../common/home.nix
-              stylix.homeManagerModules.stylix
+              #stylix.homeManagerModules.stylix
               ../../user/shell/cli-collection.nix # Useful CLI apps
               #../../user/app/doom-emacs/doom.nix # My doom emacs config
-              ../../user/style/stylix.nix # Styling and themes for my apps
-              ../../user/lang/cc/cc.nix # C and C++ tools
+              #../../user/style/stylix.nix # Styling and themes for my apps
+              #../../user/lang/cc/cc.nix # C and C++ tools
             ];
 
   home.packages = with pkgs; [
     # Core
-    syncthing
+    #syncthing
 
     # Office
-    libreoffice-fresh
+    #libreoffice-fresh
 
     # Various dev packages
-    texinfo
-    libffi zlib
-    nodePackages.ungit
+    #texinfo
+    #libffi zlib
+    #nodePackages.ungit
   ];
 
-  services.syncthing.enable = true;
+  #services.syncthing.enable = true;
 
   xdg.enable = true;
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
-    music = "${config.home.homeDirectory}/Media/Music";
-    videos = "${config.home.homeDirectory}/Media/Videos";
-    pictures = "${config.home.homeDirectory}/Media/Pictures";
-    templates = "${config.home.homeDirectory}/Templates";
+    #music = "${config.home.homeDirectory}/Media/Music";
+    #videos = "${config.home.homeDirectory}/Media/Videos";
+    #pictures = "${config.home.homeDirectory}/Media/Pictures";
+    #templates = "${config.home.homeDirectory}/Templates";
     download = "${config.home.homeDirectory}/Downloads";
     documents = "${config.home.homeDirectory}/Documents";
     desktop = null;
     publicShare = null;
     extraConfig = {
       XDG_DOTFILES_DIR = "${userSettings.dotfilesDir}";
-      XDG_ARCHIVE_DIR = "${config.home.homeDirectory}/Archive";
-      XDG_ORG_DIR = "${config.home.homeDirectory}/Org";
-      XDG_BOOK_DIR = "${config.home.homeDirectory}/Media/Books";
+      #XDG_ARCHIVE_DIR = "${config.home.homeDirectory}/Archive";
+      #XDG_ORG_DIR = "${config.home.homeDirectory}/Org";
+      #XDG_BOOK_DIR = "${config.home.homeDirectory}/Media/Books";
     };
   };
   xdg.mime.enable = true;
