65cffa26e1045265d9547f88cc29b290d8878877
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-04-12 10:57:36 +0400

    Update sddm.nix
    As per warning received

--- system/wm/sddm.nix
+++ system/wm/sddm.nix
@@ -2,7 +2,7 @@
 
 {
   # Enable SDDM.
-  services.xserver.displayManager.sddm = {
+  services.displayManager.sddm = {
     enable = true;
     theme = "${import ../../themes/sddm/sddm-sugar-candy.nix { inherit pkgs; }}";
     settings = {
