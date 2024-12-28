4dae14a10f2fbfddd37e4654dc634601186a9500
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-12-28 10:24:30 +0400

    Update kdeconnect.nix

--- system/app/kdeconnect.nix
+++ system/app/kdeconnect.nix
@@ -2,6 +2,7 @@
 
 {
   # enable KdeConnect
-  environment.systemPackages = [ pkgs.kdeconnect ];
+  #environment.systemPackages = [ pkgs.kdeconnect ];
+  environment.systemPackages = [ pkgs.plasma5Packages.kdeconnect-kde ];
   programs.kdeconnect.enable = true;
 }
