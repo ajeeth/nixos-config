e84dce2ea2eaa7fc1401ce0be5c071f3725596d7
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-12-29 17:37:51 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -104,10 +104,10 @@
   };
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-stable.url = "nixpkgs/nixos-24.11";
 
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     stylix.url = "github:danth/stylix";
