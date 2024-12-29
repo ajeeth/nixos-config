606cfa408c608e31fdc9a7e2004b4b3d69432792
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2024-12-29 17:48:57 +0400

    Update home.nix

--- profiles/homelab/home.nix
+++ profiles/homelab/home.nix
@@ -3,6 +3,16 @@
 {
   imports = [
               ../common/home.nix
+              ../../user/shell/cli-collection.nix # Useful CLI apps
+              ../../user/lang/cc/cc.nix # C and C++ tools
+              ../../user/app/ssh/ssh.nix 
             ];
+
+  # Use agenix to securly deploy ssh config file
+  age.identityPaths = [ "${config.home.homeDirectory}/.ssh/${systemSettings.profile}_${userSettings.username}-id_ed25519" ];
+  age.secrets.ssh_config = {
+    file = ../../secrets/ssh_config.age;
+    path = "${config.home.homeDirectory}/.ssh/config";
+  };
 
 }
