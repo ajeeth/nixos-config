071ed134ad7a6c478be5c7b0f070ef307dcefbf8
ajeeth <ajeeth.samuel@gmail.com>
2025-07-09 01:34:25 +0400

    Corrected git username in git.nix

--- user/app/git/git.nix
+++ user/app/git/git.nix
@@ -3,7 +3,7 @@
 {
   home.packages = [ pkgs.git ];
   programs.git.enable = true;
-  programs.git.userName = userSettings.name;
+  programs.git.userName = userSettings.username;
   programs.git.userEmail = userSettings.email;
   programs.git.extraConfig = {
     init.defaultBranch = "main";
